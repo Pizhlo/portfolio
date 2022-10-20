6b9d86d3e71668069b7f88bfc06c9208a8c4bb51
pizhlo <zaush@bk.ru>
2022-10-20 11:33:02 +0300

    added view of home page

--- personal_portfolio-project/personal_portfolio/settings.py
+++ personal_portfolio-project/personal_portfolio/settings.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru"
 
 TIME_ZONE = "UTC"
 
@@ -112,6 +112,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
