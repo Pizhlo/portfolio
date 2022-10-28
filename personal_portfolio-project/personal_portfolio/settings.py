3865989e76174c2d1be543688830f2a88cf50e26
pizhlo <zaush@bk.ru>
2022-10-28 19:18:18 +0300

    added local settings; added requirements.txt

--- personal_portfolio-project/personal_portfolio/settings.py
+++ personal_portfolio-project/personal_portfolio/settings.py
@@ -120,3 +120,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('No local file. You are on production')
