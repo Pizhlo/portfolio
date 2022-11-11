f5af589c39e94ec3b5023b3a7f78360b4a696612
pizhlo <zaush@bk.ru>
2022-11-11 11:29:48 +0300

    added models description

--- personal_portfolio-project/portfolio/models.py
+++ personal_portfolio-project/portfolio/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Project(models.Model):
+    """Модель описания проекта портфолио"""
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
