13b21bc22fc94dcc20acd8cdb84c69177b44a3c2
pizhlo <zaush@bk.ru>
2022-11-11 11:14:24 +0300

    added decription to view functions

--- personal_portfolio-project/portfolio/views.py
+++ personal_portfolio-project/portfolio/views.py
@@ -3,5 +3,6 @@ from .models import Project
 
 
 def home(request):
+    """Главная страница сайта"""
     projects = Project.objects.all()
     return render(request, 'portfolio/home.html', {'obj': projects})
