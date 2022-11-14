cbda84e1c05c55ea62817f3e219cbccf98e09beb
pizhlo <zaush@bk.ru>
2022-11-14 17:20:30 +0300

    added type hinting

--- personal_portfolio-project/blog/views.py
+++ personal_portfolio-project/blog/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Blog
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
 
 
-def all_blogs(request):
+def all_blogs(request: WSGIRequest) -> HttpResponse:
+    """Возвращает из базы данных все объекты и передает в шаблон"""
     blogs = Blog.objects.order_by('-date')
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
 
 
-def detail(request, blog_id):
+def detail(request: WSGIRequest, blog_id: int) -> HttpResponse:
+    """Отображает конкретный блог"""
     blog = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'blog/detail.html', {'blog': blog})
--- personal_portfolio-project/portfolio/views.py
+++ personal_portfolio-project/portfolio/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Project
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
 
 
-def home(request):
+def home(request: WSGIRequest) -> HttpResponse:
     """Главная страница сайта"""
     projects = Project.objects.all()
     return render(request, 'portfolio/home.html', {'obj': projects})
